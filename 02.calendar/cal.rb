0b997d295d22acdf44f4c5a9d9a226b18f78ea4f
amotech <56971237+amo-tech@users.noreply.github.com>
2024-04-29 20:21:28 +0900

    日付出力と改行位置のアップデート

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,8 +5,8 @@ require 'optparse'
 def display_calendar(year, month)
   # 月の初めの曜日を取得し、カレンダーの最初の行を揃えるための空白を挿入
   first_day_of_month = Date.new(year, month, 1)
-  offset = first_day_of_month.wday # 月曜日が0、日曜日が6
-  
+  offset = first_day_of_month.wday # 日曜日が0
+
   # 月と年を表示
   month_year_str = "#{month}月 #{year}"
   puts month_year_str.center(20)
@@ -17,14 +17,13 @@ def display_calendar(year, month)
   print "   " * offset
 
   # 1日から月の最終日までの日付を出力
-  (1..(Date.new(year, month, -1).day)).each do |day|
-    if Date.today == Date.new(year, month, day)
-      print "\e[7m#{day.to_s.rjust(2)}\e[0m "
+  (Date.new(year, month, 1)..Date.new(year, month, -1)).each do |date|
+    print "\n" if date.sunday?
+    if Date.today == date
+      print "\e[7m#{date.day.to_s.rjust(2)}\e[0m "
     else
-      print "#{day.to_s.rjust(2)} "
+      print "#{date.day.to_s.rjust(2)} "
     end
-    offset = (offset + 1) % 7
-    print "\n" if offset == 0
   end
   puts "\n"
 end
